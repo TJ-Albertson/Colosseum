1a134bffd79527293f9ea1585d4d744cd843fc94
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-09-25 14:16:45 -0500

    search tmdb
    able to search movies via tmdb api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    movie_object = Search.new(params[:search])
+    movie_object = Search.new(params[:query])
     @movies = movie_object.get_movies()
   end
 end
