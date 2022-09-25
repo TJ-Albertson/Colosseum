c1e883bbf83561bed975317c9086878a06e50340
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-09-25 16:42:30 -0500

    working on user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-
-    def self.search(search)
-        if search
-          find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-        else
-
-        end
-      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   get "/profile", to: "user#profile"
   get "/users", to: "user#search"
 
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
